6e6ae26fc44a0c7ba2c9ee36451aab99cb16cc67
kathli5 <88101685+kathli5@users.noreply.github.com>
2021-07-29 12:09:41 -0400

    Add files via upload

---
+++ KWK Mobile/Intro-To-Segues/Intro-To-Segues/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Intro-To-Segues
+//
+//  Created by Katherine Li on 7/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func submitButtonTapped(_ sender: Any) {
+    }
+    
+}
+
